6d596eaaa0a1c9ac510eb1ed8166c0a0b96453cd
atrevizo <atreviz5@msudenver.edu>
2026-01-27 16:51:43 -0700

    Add HelloWorld Java file

---
+++ HelloWorld_Arieana_Trevizo.java
@@ -0,0 +1,9 @@
+package myFirstProject;
+
+public class HelloWorld_Arieana_Trevizo {
+
+	public static void main (String[] args ) {
+		System.out.print("Hello world.This is Arieana's first Java program");
+		
+	}
+}
